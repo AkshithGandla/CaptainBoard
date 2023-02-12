07b2a71f45417d4ad8a9637cc8f91471fe29b436
AkshithGandla <akshithgandla7891@gmail.com>
2023-02-12 23:33:43 +0530

    package installation and initial config

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import clientRoutes from "./routes/client";
+import generalRoutes from "./routes/general";
+import managementRoutes from "./routes/management";
+import salesRoutes from "./routes/sales";
+
+//configuration
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+//Routes
+
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
